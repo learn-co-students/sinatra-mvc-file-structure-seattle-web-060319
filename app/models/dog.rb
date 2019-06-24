d0a14963c6037e48684c47d463a5a0b6b9ca93a3
Philip Sterling <flatironschool@Flatirons-MacBook-Air-5.local>
2019-06-24 13:48:31 -0700

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    attr_reader :name, :breed, :age 
+    attr_writer :name, :age
+    @@all = []
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+        @@all << self
+    end
+    def self.all 
+        @@all
+    end
+end
